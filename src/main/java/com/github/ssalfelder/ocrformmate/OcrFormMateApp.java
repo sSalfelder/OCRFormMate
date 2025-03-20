0204a662bae30c8c7945a184bbfac7e4d4de077d
sSalfelder <sascha.salfelder@gmx.de>
2025-03-20 19:44:18 +0100

    added configuration and vm-option to make the mainclass runnable without javafx:run

--- src/main/java/com/github/ssalfelder/ocrformmate/OcrFormMateApp.java
+++ src/main/java/com/github/ssalfelder/ocrformmate/OcrFormMateApp.java
@@ -1,11 +1,46 @@
 package com.github.ssalfelder.ocrformmate;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import java.io.IOException;
 
 @SpringBootApplication
-public class OcrFormMateApp {
+public class OcrFormMateApp extends Application {
+
+    private ConfigurableApplicationContext springContext;
+
+    @Override
+    public void init() throws Exception {
+        // Spring Boot-Kontext starten
+        springContext = SpringApplication.run(OcrFormMateApp.class);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                OcrFormMateApp.class.getResource("/com/github/ssalfelder/ocrformmate/fxml/main.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 650, 151);
+        stage.setTitle("OCRFormMate");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        // Beim Beenden JavaFX und Spring Boot sauber herunterfahren
+        springContext.close();
+        Platform.exit();
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(OcrFormMateApp.class, args);
+        // JavaFX-Start (ruft später init() und start() auf)
+        launch();
     }
 }
